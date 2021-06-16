f93b94507c0d863c2d3f9a467001a0071ed3af56
Zjinxing <1182999456@qq.com>
2021-06-16 08:16:19 +0800

    style: 列表样式

--- docs/.vitepress/theme/components/Home.vue
+++ docs/.vitepress/theme/components/Home.vue
@@ -1,8 +1,16 @@
 <template>
-  <ul class="doc-list">
-    <li v-for="doc in docList" :key="doc.frontMatter.title">
-      <a :href="doc.regularPath">
-        {{ doc.frontMatter.title }}
+  <ul class="article-list">
+    <li v-for="art in artList" :key="art.frontMatter.title"  class="article">
+      <a :href="art.regularPath">
+        <div class="article-header">
+          <h3 class="article-title">
+            {{ art.frontMatter.title }}
+          </h3>
+          <span class="article-date">{{ art.frontMatter.date }}</span>
+        </div>
+        <div class="article-describe">
+          {{ art.frontMatter.describe }}
+        </div>
       </a>
     </li>
   </ul>
@@ -13,14 +21,31 @@ import { computed } from 'vue'
 import { useSiteData } from 'vitepress'
 
 const siteDate = useSiteData()
-const docList = computed(() =>
+const artList = computed(() =>
   siteDate.value.themeConfig.pages.filter((item) => item.frontMatter.title)
 )
-console.log({ docList })
 </script>
 
 <style scoped>
-.doc-list {
+.article-list {
   list-style: none;
+  width: 650px;
+  margin: 0 auto;
+}
+.article {
+  padding-bottom: 15px;
+  margin-bottom: 15px;
+  border-bottom: 1px solid #ccc;
+}
+.article-list a {
+  text-decoration: none;
+}
+.article-header {
+  display: flex;
+  justify-content: space-between;
+  align-items: baseline;
+}
+.article-title {
+  margin: 0;
 }
 </style>
