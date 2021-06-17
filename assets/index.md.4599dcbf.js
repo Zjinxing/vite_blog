83a8e198f6fccf4feeb937fb998bbc08a63bf8f9
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-06-17 12:52:19 +0000

    Deploy Zjinxing/vite_blog to github.com/Zjinxing/vite_blog.git:gh-pages

---
+++ assets/index.md.4599dcbf.js
@@ -0,0 +1,1 @@
+import{o as e,c as t}from"./app.52d26a20.js";const r='{"title":"Home","description":"","frontmatter":{"page":true,"home":true,"date":"2021-01-03T00:00:00.000Z","title":"首页","describe":"首页"},"relativePath":"index.md","lastUpdated":1623934310765}',a={};a.render=function(r,a,d,o,i,n){return e(),t("div")};export default a;export{r as __pageData};
--- assets/tags_index.md.688506ff.lean.js
+++
@@ -1,1 +0,0 @@
-import{r as t,o as a,c as e,a as r}from"./app.2ef818da.js";const s='{"title":"分类","description":"","frontmatter":{"title":"分类"},"relativePath":"tags/index.md","lastUpdated":1623932296172}',n={};n.render=function(s,n,o,d,i,l){const p=t("Tags");return a(),e("div",null,[r(p)])};export default n;export{s as __pageData};
