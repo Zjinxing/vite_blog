5f7ae3c311010590685cc903b95753836caee8c0
Zjinxing <1182999456@qq.com>
2021-06-17 16:04:23 +0800

    update deploy

--- deploy.sh
+++ deploy.sh
@@ -17,6 +17,6 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
-git push -f https://${GITHUB_TOKEN}@github.com/Zjinxing/Zjinxing.github.io master
+git push -f https://github.com/Zjinxing/Zjinxing.github.io.git master
 
 cd -
