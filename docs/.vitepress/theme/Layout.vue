8e0be6874b1804b0d67b46836f37147776061232
Zjinxing <1182999456@qq.com>
2021-06-17 14:26:35 +0800

    自定义header

--- docs/.vitepress/theme/Layout.vue
+++ docs/.vitepress/theme/Layout.vue
@@ -1,16 +1,14 @@
 <script setup>
 import DefaultTheme from 'vitepress/theme'
 import Home from './components/Home.vue'
+import HomeHeader from './components/HomeHeader.vue'
 const { Layout } = DefaultTheme
 </script>
 
 <template>
   <Layout>
-    <template #sidebar-top>
-      My custom sidebar top content
-    </template>
-    <template #page-top>
-      <div>HOME HERO</div>
+    <template #home-hero>
+      <home-header />
     </template>
     <template #home-features>
       <Home />
---
+++ docs/.vitepress/theme/components/HomeHeader.vue
@@ -0,0 +1,20 @@
+<template>
+  <header class="home-hero">
+    <h1 class="title">WelCome!</h1>
+    <!-- TODO: header -->
+    <p class="description">HEADER 先占坑</p>
+  </header>
+</template>
+
+<script setup>
+</script>
+
+<style lang="stylus" scoped>
+.home-hero
+  height: 150px
+  margin-top: 60px
+  .title
+    text-align center
+  .description
+    text-align center
+</style>
