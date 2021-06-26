144acabc4ced20658d700c77766eb1e9ef8db67b
Zjinxing <1182999456@qq.com>
2021-06-26 15:30:24 +0800

    add CNAME

--- deploy.sh
+++ deploy.sh
@@ -11,7 +11,7 @@ cd docs/.vitepress/dist
 
 # if you are deploying to a custom domain
 # echo 'www.example.com' > CNAME
-# echo 'blog.zhaojinxing.com' > CNAME
+echo 'blog.zhaojinxing.com' > CNAME
 
 git init
 git add -A
